3e03d30b355dcb0967c6a95690d152bc76d750f3
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-05-19 15:31:40 +0530

    basic creation and traversal done

---
+++ Linked List/code.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+void traverseLL(Node* head){
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return;
+}
+
+int main(){
+
+    vector<int> arr = {2, 5, 8, 10, 12};
+
+    Node* head = convertArr2LL(arr);
+
+    traverseLL(head);    
+
+    return 0;
+}
