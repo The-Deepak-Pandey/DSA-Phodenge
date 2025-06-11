6907b2a3827e35bac6fabf1afdbe67e1e8f25ec1
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-06-11 21:28:53 +0530

    added description

--- Graphs/dsu.cpp
+++ Graphs/dsu.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 class DisjointSet {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
+    // rank: to keep track of the depth of trees
+    // parent: to keep track of the parent of each node
+    // size: to keep track of the size of each set
 
 public:
 
     DisjointSet(int n){
         rank.resize(n+1, 0);
         parent.resize(n+1);
+        size.resize(n+1, 1); // Initialize size of each set to 1
         for(int i = 0; i <= n; i++) {
             parent[i] = i; // Initially, each node is its own parent
         }
@@ -37,5 +41,21 @@ public:
             rank[ulp_u]++; // Increase the rank of the new parent
         }
     }
-    
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u); // Find the ultimate parent of u
+        int ulp_v = findUPar(v); // Find the ultimate parent of v
+
+        if(ulp_u == ulp_v) return; // If they are already in the same set, do nothing
+        
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; // Make ulp_v the parent of ulp_u
+        } else if(rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u; // Make ulp_u the parent of ulp_v
+        } else {
+            parent[ulp_v] = ulp_u; // Make ulp_u the parent of ulp_v
+            rank[ulp_u]++; // Increase the rank of the new parent
+        }
+    }
+
 };
